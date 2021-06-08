6fb11dd3827590860326bd84f4506087714d35ea
gyroskan <gyroskan@gmail.com>
2021-06-08 19:05:46 +0200

    dRoles controller completed
    Signed-off-by: gyroskan <gyroskan@gmail.com>

--- api/role.go
+++ api/role.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gyroskan/cardinal/db"
@@ -15,6 +16,8 @@ func initRoles() {
 	r.GET("/", getRoles).Name = "Fetch all guild roles."
 	r.GET("/:id", getRole).Name = "Fetch a guild role."
 	r.POST("/", createRole).Name = "Create a guild role."
+	r.PATCH("/:id", updateRole).Name = "Update a guild role."
+	r.DELETE("/:id", deleteRole).Name = "Delete a guild role."
 }
 
 // @Summary Get Guild roles
@@ -54,7 +57,7 @@ func getRole(c echo.Context) error {
 	roleID := c.Param("id")
 	var role models.Role
 
-	err := db.DB.Get(&role, "SELECT * FROM role WHERE guild_id=?,role_id", guildID, roleID)
+	err := db.DB.Get(&role, "SELECT * FROM role WHERE guild_id=?,role_id=?", guildID, roleID)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -95,4 +98,77 @@ func createRole(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusCreated, role)
+}
+
+// @Summary Update role values
+// @Tag Roles
+// @Description Update fields of a guild's role
+// @Accept  json
+// @Produce  json
+// @Param	guildID 	path	string	true	"Guild id"
+// @Param	roleID	path	string	true	"role id"
+// @Success 200 "OK" {object} models.Role
+// @Failure 403	"Forbidden"
+// @Failure 404	"Not Fountd"
+// @Failure 500 "Server Error"
+// @Router /guilds/{guildID}/roles/{roleID} [PATCH]
+func updateRole(c echo.Context) error {
+	guildID := c.Param("guildID")
+	roleID := c.Param("id")
+	var role models.Role
+
+	err := db.DB.Get(&role, "SELECT * FROM role WHERE guild_id=?,role_id=?", guildID, roleID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return c.JSON(http.StatusNotFound, nil)
+		}
+		log.Warn("UpdateRole/ Error retrieving roles: ", err)
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+
+	if err := json.NewDecoder(c.Request().Body).Decode(&role); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err)
+	}
+
+	role.GuildID = guildID
+	role.RoleID = roleID
+
+	_, err = db.DB.NamedExec(models.UpdateChannelQuery, role)
+	if err != nil {
+		log.Error("UpdateRole/ Error updating role: ", err)
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+
+	return c.JSON(http.StatusOK, role)
+}
+
+// @Summary Delete guild role
+// @Tag Roles
+// @Description Delete a guild role
+// @Accept  json
+// @Produce  json
+// @Param	guildID path	string	true	"Guild id"
+// @Param	roleID	path	string	true	"role id"
+// @Success 206 "No Content"
+// @Failure 403	"Forbidden"
+// @Failure 404	"Not Fountd"
+// @Failure 500 "Server Error"
+// @Router /guilds/{guildID}/roles/{roleID} [DELETE]
+func deleteRole(c echo.Context) error {
+	guildID := c.Param("guildID")
+	roleID := c.Param("id")
+
+	res, err := db.DB.Exec("DELETE FROM role WHERE guild_id = ?,role_id = ?", guildID, roleID)
+
+	if err != nil {
+		log.Error("HardDeleteRole/ Error while deleting role from db: ", err)
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": "Could not delete the role."})
+	}
+
+	if r, _ := res.RowsAffected(); r == 0 {
+		return c.JSON(http.StatusNotFound, nil)
+	}
+
+	return c.JSON(http.StatusNoContent, nil)
 }
