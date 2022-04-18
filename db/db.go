3a3ab84ed75fc2884b89b6b550d7f42120c846e7
baptiste nehme <baptiste.nehme@epita.fr>
2022-04-18 07:16:44 +0200

    refactor(db): connection sleep on fail for 5 attempts

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/gommon/log"
@@ -13,16 +14,30 @@ var (
 )
 
 func Connect() {
+	tries := 0
+	for tries < 5 {
+		tries++
+		if connect(os.Getenv("DB_USER"), os.Getenv("DB_PWD"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME")) {
+			return
+		}
+		time.Sleep(5 * time.Second)
+	}
+
+	log.Fatalf("Could not connect to database after %d tries.", tries)
+}
+
+func connect(db_user string , db_pwd string , db_host string, db_name string) bool {
 	log.Info("Connecting to database...")
 	var err error
-	dbString := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PWD") + "@tcp(" + os.Getenv("DB_HOST") + ")/" + os.Getenv("DB_NAME") + "?parseTime=true"
-	fmt.Println(dbString)
-	DB, err = sqlx.Open("mysql", dbString)
+	dbString := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&multiStatements=true", db_user, db_pwd, db_host, db_name)
+	log.Debug("Connecting with string: ", dbString)
+	DB, err = sqlx.Connect("mysql", dbString)
 	if err != nil {
-		log.Fatal("Connection to database failed: ", err)
-		return
+		log.Error("Connection to database failed: ", err)
+		return false
 	}
 	log.Info("Connected to database.")
+	return true
 }
 
 func Close() {
